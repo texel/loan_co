75c9aec12dbacb76f23826522f32494d65791b98
Leigh Caplan <lcaplan@onehub.com>
2010-03-21 17:37:54 -0700

    Implementing LoanApplicationsController

--- app/controllers/loan_applications_controller.rb
+++ app/controllers/loan_applications_controller.rb
@@ -1,2 +1,13 @@
 class LoanApplicationsController < ApplicationController
+  def new; end
+  
+  def create
+    @loan_application = LoanApplication.new(params[:loan_application])
+    if @loan_application.save
+      session[:loan_application_id] = @loan_application.id
+      render :nothing => true
+    else
+      render 'new'
+    end
+  end
 end
--- spec/controllers/loan_applications_controller_spec.rb
+++ spec/controllers/loan_applications_controller_spec.rb
@@ -11,6 +11,27 @@ describe LoanApplicationsController do
   end
   
   describe "#create" do
+    context "with invalid parameters" do
+      it "should render new" do
+        post :create
+        response.should render_template('new')
+      end
+    end
     
+    context "with valid params" do
+      def post_create
+        post :create, :loan_application => Factory.attributes_for(:loan_application)
+      end
+      
+      it "should create a LoanApplication" do
+        expect { post_create }.to change { LoanApplication.count }.by(1)
+      end
+      
+      it "should set the session[:loan_application_id]" do
+        post_create
+        session[:loan_application_id].should be_present
+      end
+    end
+
   end
 end
